2e6725d374e69b94933dd00a407402d27870f56e
Bouhdiallo <bouhdiallo22@gmail.com>
2024-06-09 18:17:46 +0000

    mis en place du fichier css et html

---
+++ script.js
@@ -0,0 +1,10 @@
+let infos = document.querySelector("section")
+
+var capitalDiv = document.createElement("div")
+var flagDiv = document.createElement("div")
+var countryDiv = document.createElement("div")
+
+
+function search() {
+    alert('je taime Sokhna Khady Fall')
+}
